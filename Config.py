2fddb2b3bda2d84ffa85f566eeb2cd2db9457d84
kazmabbas <105518597+kazmabbas@users.noreply.github.com>
2023-12-02 03:24:41 +0300

    لبيك ياعلي

--- Config.py
+++ Config.py
@@ -1,15 +1,15 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "27501506"))
-    API_HASH = os.environ.get("API_HASH", "30c400c8703260a92bc5ebd00422c903")
+    API_ID = int(os.environ.get("APP_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    STRING_SESSION = os.environ.get("STRING_SESSION", "BAGjo8IAApnHj2nfaDHgAtFnGAZiQbySwRszECWfhXn9Vi118dkxEgr-rI0ZA8FZL9xXcrf9Jqz6xs1czhNlrtZtYMW1hZYjfqy49BrHjZzvsjBrNqMN4d_kanuSbZAcMPDjmsW9UhB-8W4j4x_2UbBF9u4ZlwvAbWdCfJERxhRTccXvMYU6fQAYeDO1MAOXL-c4HEcY6amfWbx5Y5CxH2Rz3jubyYWS6LKV4NZyuq_aFig77ZRacMosoMp8J7mfseNRLJJxB1q1o_yuSijkj3hZMVtgzAVJATy92BIsdh4RWKm7yUnJmSCxc86GgXgvtyxE_8BFksJcUiAIvwJoRkNQICWk1gAAAAGBOlRxAA")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
     SUPPORT = os.environ.get("SUPPORT", "TUFISUN")
     CHANNEL = os.environ.get("CHANNEL", "TUFISUN")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/7c7e961ec6d48c8c3f6e6.jpg")
-    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/3d4e9709614892e62a969.jpg")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/b05bb947692ecc3ce958b.jpg")
     OWNER_ID = os.environ.get("OWNER_ID", "")
 
